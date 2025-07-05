79e436fd7d1d6242e0e45cfbb9dc05debbd8cbeb
isava777 <isava2007@gmail.com>
2025-07-05 18:15:14 +0500

    добавление ксс в другую ветку

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.example.demo to javafx.fxml;
+    opens com.example.demo to javafx.fxml, javafx.graphics;
     exports com.example.demo;
+
 }
